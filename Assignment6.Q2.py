506b6c5372938f2aba0aca219934a6832890dcab
Lavanya2227 <136583131+Lavanya2227@users.noreply.github.com>
2023-07-21 23:51:49 +0530

    Add files via upload

---
+++ Assignment6.Q2.py
@@ -0,0 +1,47 @@
+# 2.  .Assignment on creation of dog class
+
+
+class Dog:
+    def __init__(self, name, age, coat_color):
+        self.name = name
+        self.age = age
+        self.coat_color = coat_color
+
+    def description(self):
+        print("Name of Dog: ",self.name)
+        print("Age of Dog: ", self.age)
+
+    def get_info(self):
+        return self.coat_color
+    
+class JackRussellTerrier(Dog):
+    def __init__(self, name, age, coat_color):
+        Dog.__init__(self, name, age, coat_color)
+
+    def details(self):
+        print("Name: {}".format(self.name))
+        print("Age: {} years".format(self.age))
+        print("Coat Color: {}".format(self.coat_color))
+
+class Bulldog(Dog):
+    def __init__(self, name, age, coat_color):
+        Dog.__init__(self, name, age, coat_color)
+    
+    def details(self):
+        print("Name: {}".format(self.name))
+        print("Age: {} years".format(self.age))
+        print("Coat Color: {}".format(self.coat_color))
+
+print("Please Enter Details of Jack Russel Terrier Dog.")
+name1 = input("Enter name: ")
+age1 = int(input('Enter age: '))
+coat_color1 = input("Enter Coat Color: ")
+jack_russell_terrier = JackRussellTerrier(name1,age1,coat_color1)
+jack_russell_terrier.details()
+
+print("Please Enter Details of Bull Dog.")
+name2 = input("Enter name: ")
+age2 = int(input('Enter age: '))
+coat_color2 = input("Enter Coat Color: ")
+bull_dog = Bulldog(name2, age2, coat_color2)
+bull_dog.details() 
